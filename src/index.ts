99a5386e409924e912c741e949c57804a8097528
ShaharearSabbir <imshaharear@gmail.com>
2026-07-18 11:58:58 +0600

    daabase schema for match and commentary

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import 'dotenv/config'
 import express from 'express'
+import { PrismaClient } from './generated/prisma/client.js'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+})
 
 const app = express()
 const port = 8000
@@ -7,6 +14,11 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello from sportz!')
+})
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
 })
 
 app.listen(port, () => {
