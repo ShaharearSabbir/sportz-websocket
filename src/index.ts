36dddac875f78d385753d9e8b81b3d8fa657ec45
ShaharearSabbir <imshaharear@gmail.com>
2026-07-18 17:07:54 +0600

    implement web socket server

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,20 @@ import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { matchRouter } from "./modules/match/match.route.js";
 import { globalErrorHandler } from "./middleware/globalErrorHandler.js";
+import http from "http";
+import { hostname } from "zod";
+import { attachWebSocketServer } from "./utils/wsServer.js";
 
 const prisma = new PrismaClient({
   adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
 });
 
+const PORT = Number(process.env.PORT) || 8000;
+const HOST = process.env.HOST || "0.0.0.0";
+
 const app = express();
-const port = 8000;
+
+const server = http.createServer(app);
 
 app.use(express.json());
 
@@ -22,6 +29,14 @@ app.use("/matches", matchRouter);
 
 // Global error handler
 app.use(globalErrorHandler);
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+
+server.listen(PORT, HOST, () => {
+  const baseURL =
+    HOST === "0.0.0.0" ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+  console.log(`Server running at ${baseURL}`);
+  console.log(`Websocket runnding on ${baseURL.replace("http", "ws")}/ws`);
 });
---
+++ src/utils/wsServer.ts
@@ -0,0 +1,37 @@
+import { WebSocket, WebSocketServer } from "ws";
+
+export const sendJsonWS = (ws: WebSocket, data: any) => {
+  if (ws.readyState !== ws.OPEN) return;
+
+  ws.send(JSON.stringify(data));
+};
+
+export const broadcastJsonWS = (wss: WebSocketServer, data: any) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState !== client.OPEN) return;
+
+    client.send(JSON.stringify(data));
+  });
+};
+
+export const attachWebSocketServer = (
+  server: any,
+): { broadcastMatchCreated: (match: any) => void } => {
+  const wss = new WebSocketServer({
+    server,
+    path: "/ws",
+    maxPayload: 1024 * 1024,
+  }); // 1 MB
+
+  wss.on("connection", (socket) => {
+    sendJsonWS(socket, { message: "Welcome to the WebSocket server!" });
+
+    socket.on("error", console.error);
+  });
+
+  const broadcastMatchCreated = (match: any) => {
+    broadcastJsonWS(wss, { type: "matchCreated", match });
+  };
+
+  return { broadcastMatchCreated };
+};
